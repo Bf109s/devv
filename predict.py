35b5dacaecd39b8cd69562245287f6ec348fa44d
Bf109s <103197625+Bf109s@users.noreply.github.com>
2022-07-25 06:13:50 +0800

    123

--- predict.py
+++ predict.py
@@ -185,7 +185,7 @@ if __name__ == "__main__":
 #             elif selecte1 == "视频":
 #                 # st.video("./视频/地震.mp4")
 #                 st.title("视频")
-        elif choose == "防鸟装置介绍":
+        elif choose == "防鸟装置":
             st.title(':baby_chick:防鸟装置介绍\n你好 :sunglasses:') 
             Options = ["防鸟刺","防鸟挡板","防鸟盒","防鸟针板","防鸟罩","防鸟护套","防鸟拉线","人造鸟巢","人造栖鸟架","惊鸟装置","驱鸟装置"]
             choose = st.selectbox("在这里选择您想要了解的防鸟装置", Options)
